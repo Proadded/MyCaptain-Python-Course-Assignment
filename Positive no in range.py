93aef272ed8eb4b66876712e42e0efd8a923e449
Aditya Verma <128462576+Proadded@users.noreply.github.com>
2023-03-29 14:23:19 +0530

    Identification of positive no. present in range

---
+++ Positive no in range.py
@@ -0,0 +1,16 @@
+l1=[12,-7,5,64,-14]
+l2=[12,14,-95,3]
+l3=[]
+l4=[]
+for i in l1:
+    if i>0:
+        e=i
+        l3.append(e)
+for k in l2:
+    if k>0:
+        f=k
+        l4.append(k)
+print("Input: list1 =",l1)
+print("Output:", l3)
+print("Input: list3 =",l2)
+print("Output:",l4)
